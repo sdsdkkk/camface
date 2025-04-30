e6bda6b5c63c9c370a42eed0dc02281099bcefcb
sdsdkkk <vcc.edwint@gmail.com>
2025-04-30 08:27:30 +0700

    detectFace() should return void since the image frame is mutated inside the function

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 #include <opencv2/opencv.hpp>
 #include <iostream>
 
-cv::Mat detectFace(cv::Mat image);
+void detectFace(cv::Mat image);
 
 int main() {
     cv::VideoCapture cap(0);
@@ -19,7 +19,8 @@ int main() {
             break;
         }
 
-        cv::imshow("Camface", detectFace(frame));
+        detectFace(frame);
+        cv::imshow("Camface", frame);
         if (cv::waitKey(1) == 27) break; // Press ESC to exit
     }
 
@@ -28,27 +29,25 @@ int main() {
     return 0;
 }
 
-cv::Mat detectFace(cv::Mat image) {
+void detectFace(cv::Mat image) {
     cv::Mat grayscaleImage;
 
     cv::cvtColor(image, grayscaleImage, cv::COLOR_BGR2GRAY);
 
     cv::CascadeClassifier faceCascade;
-    if(!faceCascade.load("haarcascade_frontalface_alt.xml")){
+    if (!faceCascade.load("haarcascade_frontalface_alt.xml")) {
         std::cerr << "Error: Haar Cascade loading failed" << std::endl;
-        return image;
+        return;
     }
 
     std::vector<cv::Rect> faces;
     faceCascade.detectMultiScale(grayscaleImage, faces, 1.1, 3, 0, cv::Size(30, 30));
 
-    for (const auto& face : faces){
+    for (const auto& face : faces) {
       cv::rectangle(image, face, cv::Scalar(255, 0, 0), 2);
 
       if (face.width > 100) {
           cv::putText(image, "SHOOT!", cv::Point(face.x, face.y - 10), cv::FONT_HERSHEY_SIMPLEX, 0.9, cv::Scalar(255, 0, 0), 2);
       }
     }
-
-    return image;
 }
